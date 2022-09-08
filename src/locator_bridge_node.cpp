250d4705a50c75458c927cbe9ca9cc331fcda955
Stefan Laible <stefan.laible@de.bosch.com>
2022-09-08 11:54:24 +0200

    Rebase from 'release/noetic/bosch_locator_bridge'

--- src/locator_bridge_node.cpp
+++ src/locator_bridge_node.cpp
@@ -15,7 +15,6 @@
 
 #include "locator_bridge_node.hpp"
 
-#include "locator_rpc_interface.hpp"
 #include "sending_interface.hpp"
 #include "receiving_interface.hpp"
 #include "rosmsgs_datagram_converter.hpp"
@@ -25,26 +24,27 @@
 #include <tf2/convert.h>
 
 /// locator module versions to check against. Format is name, { major_version, minor_version }
-const static std::unordered_map<std::string, std::pair<int32_t, int32_t>> REQUIRED_MODULE_VERSIONS({
-    { "AboutModules", { 5, 0 } },
-    { "Session", { 3, 0 } },
-    { "Diagnostic", { 4, 0 } },
-    { "Licensing", { 6, 0 } },
-    { "Config", { 5, 0 } },
-    { "AboutBuild", { 3, 0 } },
-    { "Certificate", { 3, 0 } },
-    { "System", { 3, 1 } },
-    { "ClientApplication", { 1, 0 } },
-    { "ClientControl", { 3, 1 } },
-    { "ClientRecording", { 3, 2 } },
-    { "ClientMap", { 3, 3 } },
-    { "ClientLocalization", { 5, 1 } },
-    { "ClientManualAlign", { 4, 1 } },
-    { "ClientGlobalAlign", { 4, 0 } },
-    { "ClientLaserMask", { 5, 0 } },
-    { "ClientSensor", { 5, 0 } },
-    { "ClientUser", { 4, 0 } },
-    { "ClientExpandMap", { 1, 0 } },
+static const std::unordered_map<std::string, std::pair<int32_t, int32_t>> REQUIRED_MODULE_VERSIONS({
+  {"AboutModules", {5, 0}},
+  {"Session", {3, 1}},
+//  {"Diagnostic", {4, 0}},
+  {"Licensing", {6, 1}},
+  {"Config", {5, 0}},
+  {"AboutBuild", {3, 0}},
+  {"Certificate", {3, 0}},
+  {"System", {3, 1}},
+//  {"ClientApplication", {1, 0}},
+  {"ClientControl", {3, 1}},
+  {"ClientRecording", {4, 0}},
+  {"ClientMap", {4, 0}},
+  {"ClientLocalization", {6, 0}},
+//  {"ClientManualAlign", {5, 0}},
+  {"ClientGlobalAlign", {4, 0}},
+//  {"ClientLaserMask", {5, 0}},
+  {"ClientSensor", {5, 1}},
+//  {"ClientUser", {4, 0}},
+//  {"User", {1, 0}},
+//  {"ClientExpandMap", {2, 0}},
 });
 
 LocatorBridgeNode::LocatorBridgeNode() : nh_("~")
@@ -209,7 +209,10 @@ void LocatorBridgeNode::laser_callback(const sensor_msgs::LaserScan& msg)
   }
 
   Poco::Buffer<char> laserscan_datagram = RosMsgsDatagramConverter::convertLaserScan2DataGram(msg, ++scan_num_, scan_time);
-  laser_sending_interface_->sendData(laserscan_datagram.begin(), laserscan_datagram.size());
+  if (laser_sending_interface_->sendData(laserscan_datagram.begin(), laserscan_datagram.size()) == SendingInterface::SendingStatus::IO_EXCEPTION)
+  {
+    checkLaserScan(msg, "laser");
+  }
 }
 
 void LocatorBridgeNode::laser2_callback(const sensor_msgs::LaserScan& msg)
@@ -226,7 +229,10 @@ void LocatorBridgeNode::laser2_callback(const sensor_msgs::LaserScan& msg)
   }
 
   Poco::Buffer<char> laserscan_datagram = RosMsgsDatagramConverter::convertLaserScan2DataGram(msg, ++scan2_num_, scan_time);
-  laser2_sending_interface_->sendData(laserscan_datagram.begin(), laserscan_datagram.size());
+  if (laser2_sending_interface_->sendData(laserscan_datagram.begin(), laserscan_datagram.size()) == SendingInterface::SendingStatus::IO_EXCEPTION)
+  {
+    checkLaserScan(msg, "laser2");
+  }
 }
 
 void LocatorBridgeNode::odom_callback(const nav_msgs::Odometry& msg)
@@ -238,16 +244,7 @@ void LocatorBridgeNode::odom_callback(const nav_msgs::Odometry& msg)
 bool LocatorBridgeNode::clientConfigGetEntryCb(bosch_locator_bridge::ClientConfigGetEntry::Request& req,
                                                bosch_locator_bridge::ClientConfigGetEntry::Response& res)
 {
-  const auto & loc_client_config = loc_client_interface_->getConfigList();
-
-  try {
-    res.value = loc_client_config[req.name].toString();
-  } catch (const Poco::NotFoundException & error) {
-    ROS_ERROR_STREAM("Could not find config entry " << req.name << ".");
-    return false;
-  }
-
-  return true;
+  return get_config_entry(req.name, res.value);
 }
 
 bool LocatorBridgeNode::clientMapSendCb(bosch_locator_bridge::ClientMapSend::Request& req,
@@ -398,7 +395,41 @@ void LocatorBridgeNode::syncConfig()
       case XmlRpc::XmlRpcValue::TypeString:
         loc_client_config[key] = static_cast<std::string>(value);
         break;
-      // TODO: add missing parameters
+      case XmlRpc::XmlRpcValue::TypeArray:
+        if (value.size() == 0)
+        {
+          ROS_WARN_STREAM("empty array for " << key);
+        }
+        else
+        {
+          switch (value[0].getType())
+          {
+            case XmlRpc::XmlRpcValue::TypeBoolean:
+            {
+              loc_client_config[key] = convert_value_array_to_vector<bool>(value);
+              break;
+            }
+            case XmlRpc::XmlRpcValue::TypeInt:
+            {
+              loc_client_config[key] = convert_value_array_to_vector<int>(value);
+              break;
+            }
+            case XmlRpc::XmlRpcValue::TypeDouble:
+            {
+              loc_client_config[key] = convert_value_array_to_vector<double>(value);
+              break;
+            }
+            case XmlRpc::XmlRpcValue::TypeString:
+            {
+              loc_client_config[key] = convert_value_array_to_vector<std::string>(value);
+              break;
+            }
+            default:
+              ROS_ERROR_STREAM("unknown element type for " << key);
+              break;
+          }
+        }
+        break;
       default:
         ROS_ERROR_STREAM("unknown config type for " << key);
         break;
@@ -449,6 +480,34 @@ void LocatorBridgeNode::syncConfig()
   }
 
   loc_client_interface_->setConfigList(loc_client_config);
+}
+
+void LocatorBridgeNode::checkLaserScan(const sensor_msgs::LaserScan& msg,
+                                       const std::string& laser) const
+{
+  if (fabs(msg.angle_min + (msg.ranges.size() - 1) * msg.angle_increment - msg.angle_max) >
+    fabs(0.5 * msg.angle_increment))
+  {
+    ROS_ERROR_STREAM(
+      "LaserScan message is INVALID: " << msg.angle_min << " (angle_min) + " <<
+      (msg.ranges.size() - 1) << " (ranges.size - 1) * " << msg.angle_increment <<
+        " (angle_increment) = " <<
+      (msg.angle_min + (msg.ranges.size() - 1) * msg.angle_increment) << ", expected " <<
+        msg.angle_max << " (angle_max)");
+  }
+  else
+  {
+    const std::string param_name = "ClientSensor." + laser + ".useIntensities";
+    std::string laser_use_intensities;
+    if (get_config_entry(
+        param_name, laser_use_intensities) &&
+      laser_use_intensities == "true" && msg.ranges.size() != msg.intensities.size())
+    {
+      ROS_ERROR_STREAM(
+        "LaserScan message is INVALID: " << param_name << " is true, but ranges.size (" <<
+          msg.ranges.size() << ") unequal intensities.size (" << msg.intensities.size() << ")");
+    }
+  }
 }
 
 void LocatorBridgeNode::setupBinaryReceiverInterfaces(const std::string& host)
--- src/receiving_interface.cpp
+++ src/receiving_interface.cpp
@@ -54,8 +54,15 @@ void ReceivingInterface::onReadEvent(const Poco::AutoPtr<Poco::Net::ReadableNoti
     else
     {
       datagram_buffer_.insert(datagram_buffer_.end(), msg.begin(), msg.end());
-      const auto bytes_to_delete = tryToParseData(datagram_buffer_);
-      datagram_buffer_.erase(datagram_buffer_.begin(), datagram_buffer_.begin() + bytes_to_delete);
+
+      size_t bytes_to_delete = 0;
+      // Try to parse messages from the buffer until tryToParseData fails to parse a full message
+      do {
+        bytes_to_delete = tryToParseData(datagram_buffer_);
+        datagram_buffer_.erase(
+          datagram_buffer_.begin(),
+          datagram_buffer_.begin() + bytes_to_delete);
+      } while (bytes_to_delete > 0);
     }
   }
   catch (const std::ios_base::failure& io_failure)
@@ -72,23 +79,6 @@ void ReceivingInterface::onReadEvent(const Poco::AutoPtr<Poco::Net::ReadableNoti
 void ReceivingInterface::run()
 {
   reactor_.run();
-}
-
-void ReceivingInterface::publishTransform(const geometry_msgs::PoseStamped& pose, const std::string& parent_frame,
-                                          const std::string child_frame)
-{
-  geometry_msgs::TransformStamped transform;
-  transform.header.stamp = pose.header.stamp;
-  transform.header.frame_id = parent_frame;
-
-  transform.transform.translation.x = pose.pose.position.x;
-  transform.transform.translation.y = pose.pose.position.y;
-  transform.transform.translation.z = pose.pose.position.z;
-  transform.transform.rotation = pose.pose.orientation;
-
-  transform.child_frame_id = child_frame;
-
-  tf_broadcaster_.sendTransform(transform);
 }
 
 ClientControlModeInterface::ClientControlModeInterface(const Poco::Net::IPAddress& hostadress, ros::NodeHandle& nh)
@@ -157,7 +147,6 @@ size_t ClientMapVisualizationInterface::tryToParseData(const std::vector<char>& 
   if (bytes_parsed > 0)
   {
     // publish
-    publishTransform(pose, MAP_FRAME_ID, LASER_FRAME_ID);
     publishers_[0].publish(client_map_visualization);
     publishers_[1].publish(pose);
     publishers_[2].publish(scan);
@@ -212,7 +201,6 @@ size_t ClientRecordingVisualizationInterface::tryToParseData(const std::vector<c
   if (parsed_bytes > 0)
   {
     // publish
-    publishTransform(pose, MAP_FRAME_ID, LASER_FRAME_ID);
     publishers_[0].publish(client_recording_visualization);
     publishers_[1].publish(pose);
     publishers_[2].publish(scan);
@@ -310,7 +298,6 @@ size_t ClientLocalizationPoseInterface::tryToParseData(const std::vector<char>& 
   if (bytes_parsed > 0)
   {
     // publish
-    publishTransform(pose, MAP_FRAME_ID, LASER_FRAME_ID);
     publishers_[0].publish(client_localization_pose);
     publishers_[1].publish(poseWithCov);
     publishers_[2].publish(lidar_odo_pose);
