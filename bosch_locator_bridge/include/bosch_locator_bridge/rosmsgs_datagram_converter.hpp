3e78ccc4134b2a4d9b46451d52a24cdf0b03d29a
Stefan Laible <stefan.laible@de.bosch.com>
2022-09-08 11:54:06 +0200

    Rebase from 'upstream'

--- bosch_locator_bridge/include/bosch_locator_bridge/rosmsgs_datagram_converter.hpp
+++ bosch_locator_bridge/include/bosch_locator_bridge/rosmsgs_datagram_converter.hpp
@@ -38,7 +38,6 @@
 
 #define MAP_FRAME_ID "map"
 #define ODOM_FRAME_ID "odom"
-#define LASER_FRAME_ID "base_laser"
 
 /**
  * Class with static function to convert ros messages to locator's datagrams.
@@ -179,6 +178,7 @@ private:
                                               pcl::PointCloud<pcl::PointXYZRGB>& out_pointcloud);
   static void colorizePointCloud(pcl::PointCloud<pcl::PointXYZRGB>& point_cloud,
                                  const std::vector<uint64_t>& sensor_offsets);
+  static size_t discardExtension(Poco::BinaryReader& binary_reader);
   static void readIntensities(Poco::BinaryReader& binary_reader);
   static std::vector<uint64_t> readSensorOffsets(Poco::BinaryReader& binary_reader);
 
